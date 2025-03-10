a1186194f21403e44e718c089d9f7897b618d9b2
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-03-10 14:56:21 +0100

    fix: Correct some job missing a semi colon

--- job10.sql
+++ job10.sql
@@ -40,8 +40,7 @@ LOCK TABLES `etudiant` WRITE;
 /*!40000 ALTER TABLE `etudiant` DISABLE KEYS */;
 INSERT INTO `etudiant` VALUES (1,'Spaghetti','Betty',23,'betty.Spaghetti@laplateforme.io'),(2,'Steak','Chuck',45,'chuck.steak@laplateforme.io'),(3,'Doe','John',18,'john.doe@laplateforme.io'),(4,'Barnes','Binkie',16,'binkie.barnes@laplateforme.io'),(5,'Dupuis','Gertrude',20,'gertrude.dupuis@laplateforme.io');
 
-SELECT * FROM etudiant
-ORDER BY age DESC;
+SELECT * FROM etudiant ORDER BY age DESC;
 /*!40000 ALTER TABLE `etudiant` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
